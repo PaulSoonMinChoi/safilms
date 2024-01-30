7f345ba6ac14b85fc6e14b803044d9c67a090e58
paulsoonminchoi <paulsoonminchoi@gmail.com>
2024-01-30 11:13:09 -0800

    First commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,135 @@
+"use client";
 import Image from "next/image";
+// core version + navigation, pagination modules:
+import { Swiper, SwiperSlide } from "swiper/react";
+import {
+  Navigation,
+  Mousewheel,
+  Pagination,
+  Scrollbar,
+  Parallax,
+} from "swiper/modules";
+// import Swiper and modules styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <Swiper
+      // className="swiper-container"
+      slidesPerView={1}
+      navigation
+      mousewheel={true}
+      pagination={{ type: "bullets", clickable: true }}
+      modules={[Navigation, Pagination, Mousewheel]}
+      loop={true}
+      speed={800}
+    >
+      <SwiperSlide>
+        <div className="w-[100vw] h-[100vh]">
+          <video
+            className="video-test"
+            loop
+            muted
+            autoPlay
+            playsInline
+            controls={undefined}
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            <source src="/vid/deathvalleytrailer.mp4" type="video/mp4" />
+          </video>
+          <div className="flex justify-center items-center w-[100vw] pt-6">
+            <Link href="/">
+              <Image
+                className="logo"
+                // src={`${main ? "/icons/ov.svg" : "/icons/ovblack.svg"}`}
+                src={"/salogo.png"}
+                alt="SA logo"
+                width={80}
+                height={80}
+              />
+            </Link>
+          </div>
+
+          <div className="flex justify-center items-center text-center w-[100vw] p-[100px]">
+            <span>SA Films is a production of films by Soul Assassins</span>
+          </div>
+
+          <div className="bottom-info w-[100vw]">
+            <small>
+              © 2024,{" "}
+              <Link className="home-link" href="https://soulassassins.com/">
+                Soul Assassins
+              </Link>
+            </small>
+          </div>
+
+          {/* <div className="flex justify-center items-center w-[50vw] h-[100vh]">
+            <div className="w-[40vw] h-[100vh]">
+              <iframe
+                className="w-[100%] h-[100%]"
+                src="https://player.vimeo.com/video/903023455?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
+                // frameBorder="0"
+                allow="autoplay; fullscreen; picture-in-picture"
+                title="DEATH VALLEY"
+              ></iframe>
+            </div>
+          </div> */}
         </div>
-      </div>
+      </SwiperSlide>
+      <SwiperSlide>
+        <div className="w-[100vw] h-[100vh]">
+          <div className="background-image"></div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+          <div className="flex justify-center items-center w-[100vw] pt-6">
+            <h1> DEATH VALLEY </h1>
+          </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
+          <div className="info-text-container text-center">
+            <span className="info-text">
+              A new powder has hit the streets of Oakland by way of India, and
+              every gang in the region wants a taste. From acclaimed music
+              producer DJ Muggs and Soul Assassins comes Death Valley. Directed
+              by legendary music video director Jason Goldwatch making his
+              feature film debut. And sporting an Allstar hip-hop cast with
+              appearances by Roc Marciano, Meyhem Lauren, Boldy James, and DJ
+              Muggs himself.
             </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+          </div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+          <div className="content-container flex justify-center items-center w-[100vw]">
+            <div className="content-info flex flex-col justify-center items-center w-[50vw] gap-[50px] p-[100px]">
+              <div className="text-center">
+                <span>To watch the entire Death Valley Video purchase the extended video cut.</span>
+              </div>
+              <a
+                className="button"
+                href="https://vimeo.com/store/ondemand/popup/6617208?referrer=https%3A%2F%2Fvimeo.com%2F&ssl=1"
+                target="_blank"
+              >
+                Purchase
+              </a>
+            </div>
+            <div className="vid-container w-[50vw]">
+              <iframe
+                className="w-[100%] h-[100%]"
+                src="https://player.vimeo.com/video/903023455?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
+                allow="autoplay; fullscreen; picture-in-picture"
+                title="DEATH VALLEY"
+              ></iframe>
+            </div>
+          </div>
+          <div className="bottom-info w-[100vw]">
+            <small>
+              © 2024,{" "}
+              <Link className="home-link" href="https://soulassassins.com/">
+                Soul Assassins
+              </Link>
+            </small>
+          </div>
+        </div>
+      </SwiperSlide>
+    </Swiper>
   );
 }
