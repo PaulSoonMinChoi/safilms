66440ef4eac68a1b121acc329d018e7cb23c609d
paulsoonminchoi <paulsoonminchoi@gmail.com>
2024-02-01 15:53:18 -0800

    Add new watermarks and background image

--- app/page.tsx
+++ app/page.tsx
@@ -14,14 +14,34 @@ import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [showMobileBG, setShowMobileBG] = useState(false);
+
+  useEffect(() => {
+    // Add event listener to update screen width
+    const handleResize = () => {
+      setShowMobileBG(window.innerWidth <= 600);
+    };
+
+    // Initial check for screen width
+    handleResize();
+
+    // Add resize event listener
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <Swiper
       // className="swiper-container"
       className="w-[100vw] h-[100vh]"
       slidesPerView={1}
-      navigation
+      // navigation
       mousewheel={true}
       pagination={{ type: "bullets", clickable: true }}
       modules={[Navigation, Pagination, Mousewheel]}
@@ -30,7 +50,7 @@ export default function Home() {
     >
       <SwiperSlide>
         <div className="w-[100vw] h-[100vh]">
-          <video
+          {/* <video
             className="video-test"
             loop
             muted
@@ -39,7 +59,14 @@ export default function Home() {
             controls={undefined}
           >
             <source src="/vid/deathvalleytrailer.mp4" type="video/mp4" />
-          </video>
+          </video> */}
+          {!showMobileBG ? (
+            <div className="hero-image" />
+          ) : (
+            <div className="hero-image-mobile" />
+          )}
+          {/* <div className="hero-image" /> */}
+
           <div className="flex justify-center items-center w-[100vw] pt-6">
             <Link href="/">
               <Image
@@ -53,9 +80,9 @@ export default function Home() {
             </Link>
           </div>
 
-          <div className="flex justify-center items-center text-center w-[100vw] p-[100px]">
+          {/* <div className="flex justify-center items-center text-center w-[100vw] p-[100px]">
             <span>SA Films is a production of films by Soul Assassins</span>
-          </div>
+          </div> */}
 
           <div className="bottom-info w-[100vw]">
             <small>
@@ -93,20 +120,29 @@ export default function Home() {
           <div className="background-image"></div>
 
           <div className="flex justify-center items-center w-[100vw] pt-6">
-            <h1> DEATH VALLEY </h1>
+            <Link href="/">
+              <Image
+                className="logo"
+                // src={`${main ? "/icons/ov.svg" : "/icons/ovblack.svg"}`}
+                src={"/salogo.png"}
+                alt="SA logo"
+                width={80}
+                height={80}
+              />
+            </Link>
           </div>
 
-          {/* <div className="info-text-container text-center"> */}
-          {/* <span className="info-text">
-              A new powder has hit the streets of Oakland by way of India, and
-              every gang in the region wants a taste. From acclaimed music
-              producer DJ Muggs and Soul Assassins comes Death Valley. Directed
-              by legendary music video director Jason Goldwatch making his
-              feature film debut. And sporting an Allstar hip-hop cast with
-              appearances by Roc Marciano, Meyhem Lauren, Boldy James, and DJ
-              Muggs himself.
-            </span> */}
-          {/* </div> */}
+          <div className="flex justify-center items-center w-[100vw]">
+            {/* <h1> DEATH VALLEY </h1> */}
+            <Image
+              className="mt-[30px]"
+              // src={`${main ? "/icons/ov.svg" : "/icons/ovblack.svg"}`}
+              src={"/deathvalleylogo.png"}
+              alt="SA logo"
+              width={250}
+              height={250}
+            />
+          </div>
 
           <div className="content-container flex  w-[100vw]">
             <div className="content-info flex flex-col  w-[50vw]">
@@ -132,7 +168,16 @@ export default function Home() {
 
             <div className="content-info flex flex-col justify-center items-center w-[50vw]">
               <div className="flex justify-center flex-col gap-[10px] text-center items-center">
-              <span className="info-text">DEATH VALLEY DELUXE CONTENT</span>
+                {/* <span className="info-text">DEATH VALLEY DELUXE CONTENT</span> */}
+                <Image
+                  // className="logo"
+                  className="m-[30px]"
+                  // src={`${main ? "/icons/ov.svg" : "/icons/ovblack.svg"}`}
+                  src={"/dvdeluxe.png"}
+                  alt="SA logo"
+                  width={250}
+                  height={250}
+                />
                 <div className="flex justify-center items-center">
                   <div className="vid-container w-[50vw]">
                     <iframe
@@ -144,24 +189,36 @@ export default function Home() {
                   </div>
                 </div>
                 {/* <span className="info-text">DEATH VALLEY DELUXE CONTENT</span> */}
-                <span className="sub-info-text">
-                  - Introduction by DJ MUGGS
-                </span>
-                <span className="sub-info-text">
-                  - 14 Min behind scenes extras
-                </span>
-                <span className="sub-info-text">- Promotional Art Slide</span>
-                <span className="sub-info-text">
-                  - Two theatrical trailers for the film
-                </span>
-                <span className="sub-info-text-long">
-                  - 5 Music Videos: Boldy James - Coming for the Safe (Directors
-                  Cut) Meyhem Lauren - Ready for War Roc Marciano - Crazy Horse
-                  (Directors Cut) Rome Streetz - Skeleton Bones Jay Worthy -
-                  Check in (OG Version)
-                </span>
-
-                {/* <span className="info-text"></span> */}
+                <div className="sub-info-container flex flex-col justify-center text-left">
+                  <span className="sub-info-text">
+                    - Introduction by DJ MUGGS
+                  </span>
+                  <span className="sub-info-text">
+                    - 14 Min behind scenes extras
+                  </span>
+                  <span className="sub-info-text">- Promotional Art Slide</span>
+                  <span className="sub-info-text">
+                    - Two theatrical trailers for the film
+                  </span>
+                  <span className="sub-info-text-long">
+                    - 5 Music Videos: Boldy James - Coming for the Safe
+                    (Directors Cut) Meyhem Lauren - Ready for War Roc Marciano -
+                    Crazy Horse (Directors Cut) Rome Streetz - Skeleton Bones
+                    Jay Worthy - Check in (OG Version)
+                  </span>
+                  <span className="sub-info-text">
+                    Log into your{" "}
+                    <Link
+                      className="home-link"
+                      target="_blank"
+                      rel="noreferrer"
+                      href="https://vimeo.com/"
+                    >
+                      Vimeo
+                    </Link>{" "}
+                    account to view the Deluxe Content
+                  </span>
+                </div>
                 {/* <a
                   className="button"
                   href="https://vimeo.com/store/ondemand/popup/6617208?referrer=https%3A%2F%2Fvimeo.com%2F&ssl=1"
@@ -169,28 +226,6 @@ export default function Home() {
                 >
                   Purchase
                 </a> */}
-                {/* <div className="flex justify-center items-center">
-                  <div className="vid-container w-[50vw]">
-                    <iframe
-                      className="w-[100%] h-[100%]"
-                      src="https://player.vimeo.com/video/876150152?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
-                      allow="autoplay; fullscreen; picture-in-picture"
-                      title="DEATH VALLEY"
-                    ></iframe>
-                  </div>
-                </div> */}
-                <span className="sub-info-text">
-                  Log into your{" "}
-                  <Link
-                    className="home-link"
-                    target="_blank"
-                    rel="noreferrer"
-                    href="https://vimeo.com/"
-                  >
-                    Vimeo
-                  </Link>{" "}
-                  account to view the Deluxe Content
-                </span>
               </div>
             </div>
           </div>
