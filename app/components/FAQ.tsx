317faf3d77f087f8297db1f1a76fff73c7d425a4
paulsoonminchoi <paulsoonminchoi@gmail.com>
2024-02-03 13:28:11 -0800

    Add side nav to smaller pages

--- app/components/FAQ.tsx
+++ app/components/FAQ.tsx
@@ -1,8 +1,15 @@
-import React from "react";
+'use client'
+import React, { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBarsStaggered, faX } from "@fortawesome/free-solid-svg-icons";
 
 const FAQ = () => {
+  const [showMenu, setShowMenu] = useState(false);
+  const handleSideMenuClose = () => {
+    setShowMenu(false);
+  };
   return (
     <div className="h-[100vh] overflow-hidden">
       <div className="flex justify-center items-center w-[100vw] pt-6">
@@ -12,11 +19,44 @@ const FAQ = () => {
             // src={`${main ? "/icons/ov.svg" : "/icons/ovblack.svg"}`}
             src={"/salogo.png"}
             alt="SA logo"
-            width={80}
-            height={80}
+            width={65}
+            height={65}
           />
         </Link>
       </div>
+
+      <FontAwesomeIcon
+        className={"menu-icon"}
+        icon={faBarsStaggered}
+        onClick={() => setShowMenu(true)}
+      />
+
+      {showMenu && (
+        <div className="side-navigation-container">
+          <div>
+            <FontAwesomeIcon
+              className="menu-icon"
+              icon={faX}
+              onClick={() => handleSideMenuClose()}
+            />
+          </div>
+          <div className="side-navigation">
+            <Link className="home-link" href="/contact">
+              Contact
+            </Link>
+            <Link
+              className="home-link"
+              href="https://soulassassins.com/"
+              style={{ textDecoration: "none !important" }}
+            >
+              Soul Assassins
+            </Link>
+            <Link className="home-link" href="/faq">
+              FAQ
+            </Link>
+          </div>
+        </div>
+      )}
 
       <div className="nav-info-text flex justify-center items-center text-center flex-col gap-[50px] h-[80vh] p-[10px]">
         <h1>FAQ</h1>
@@ -39,7 +79,8 @@ const FAQ = () => {
           <Link
             className="home-link"
             href="https://soulassassins.com/"
-            style={{ textDecoration: "none !important" }}>
+            style={{ textDecoration: "none !important" }}
+          >
             Soul Assassins
           </Link>
         </small>
